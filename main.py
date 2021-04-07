524c4fe8d499da741bf7368afddda56de8567747
bobo <77556196+opalgreenbranch@users.noreply.github.com>
2021-04-07 12:27:12 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-let x = 0
-let y = 0
-let index = 0
-for (index = 0; index < 5; index++) {
+x = 0
+y = 0
+index = 0
+
+for index in range(5):
     led.plot(x, y)
     y += 1
     led.plot(x, y)
     x += 1
     index += 1
-}
 index = 0
 y = 0
 x = 4
-for (index = 0; index < 5; index++) {
+for  index in range(5):
     led.plot(x, y)
     y += 1
     led.plot(x, y)
     x -= 1
     index += 1
-}
 index = 0
